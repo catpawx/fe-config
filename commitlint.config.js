941c6ae308ab2afb45f4eb5e23edbfda40b013bc
相甫 <xiangfu_git@163.com>
2024-06-27 21:49:34 +0800

    feat(other): commitizen生成注释,cz-git辅助流程工具添加

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,45 +1,16 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
 
-const packagesScopesLint = [
-  "babel-plugin-px2dp",
-  "eslint-config",
-  "printerjs",
-  "react-native-ui",
-  "react-native-base",
-  "react-native-printer",
-  "react-native-utils",
-  "react-native-uni",
-  "react-native-wechat",
-  "react-native-version-update",
-  "react-native-pushy",
-  "react-native-network-access-check",
-  "cashier-desk"
-];
+const packagesScopesLint = ["babel-plugin-px2dp"];
 const packagesScopes = [
-  { value: "react-native-pushy", name: "react-native-pushy                    pushy热更新" },
-  { value: "react-native-printer", name: "react-native-printer                  打印机库" },
-  { value: "printerjs", name: "printerjs                             打印机模板编译库" },
-  { value: "react-native-ui", name: "react-native-ui                       ui库" },
-  { value: "react-native-base", name: "react-native-base                     基础库" },
-  { value: "react-native-utils", name: "react-native-utils                    工具库" },
-  { value: "react-native-uni", name: "react-native-uni                      uni sdk" },
-  { value: "react-native-wechat", name: "react-native-wechat                   wechat sdk" },
-  { value: "react-native-version-update", name: "react-native-version-update           app版本更新" },
-  { value: "eslint-config", name: "eslint-config                         eslint规则" },
-  { value: "react-native-network-access-check", name: "react-native-network-access-check     ios网络权限" },
-  { value: "cashier-desk", name: "cashier-desk:                         收银台" },
-  { value: "babel-plugin-px2dp", name: "babel-plugin-px2dp                    px2dp自动转换插件" },
+  {
+    value: "babel-plugin-px2dp",
+    name: "babel-plugin-px2dp                    px2dp自动转换插件",
+  },
   {
     value: "other",
-    name: "other:                   其他，不确定选其他，在这之上选项必须精确，在这之下选项建议精确"
+    name: "other:                   其他，不确定选其他",
   },
-  { value: "docs", name: "docs:                    根目录文档" },
-  { value: "examples", name: "examples:                demo" },
-  { value: "scripts", name: "scripts:                 脚本" },
-  { value: "templates", name: "templates:               暂存的一些模板" },
-  { value: ".husky", name: ".husky:                  husky的一些配置" },
-  { value: ".vscode", name: ".vscode:                 vscode的一些配置" }
 ];
 
 module.exports = {
@@ -59,7 +30,18 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "revert", "chore"]
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "revert",
+        "chore",
+      ],
     ],
     /**
      * type[scope]: [function] description
@@ -68,7 +50,16 @@ module.exports = {
     "scope-enum": [
       2,
       "always",
-      [...packagesScopesLint, "docs", "examples", "scripts", "templates", ".husky", ".vscode", "other"]
+      [
+        ...packagesScopesLint,
+        "docs",
+        "examples",
+        "scripts",
+        "templates",
+        ".husky",
+        ".vscode",
+        "other",
+      ],
     ], //scope 可选值
     /**
      * type[scope]: [function] description
@@ -94,10 +85,14 @@ module.exports = {
     "type-empty": [2, "never"], //type不能为空
     "scope-case": [2, "always", "lower-case"], //scope 单词格式
     "scope-empty": [2, "never"], //scope不能为空
-    "subject-case": [1, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]], //subject 单词格式
+    "subject-case": [
+      1,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ], //subject 单词格式
     "subject-empty": [2, "never"], //subject 是否为空
     "subject-min-length": [2, "never", 5], //subject 至少5个字
-    "subject-full-stop": [2, "never", "."] //subject 中止符
+    "subject-full-stop": [2, "never", "."], //subject 中止符
   },
   prompt: {
     alias: { fd: "docs: fix typos" },
@@ -111,19 +106,40 @@ module.exports = {
       footerPrefixsSelect: "选择关联issue前缀（可选）:",
       customFooterPrefixs: "输入自定义issue前缀 :",
       footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
-      confirmCommit: "是否提交或修改commit ?"
+      confirmCommit: "是否提交或修改commit ?",
     },
     types: [
       { value: "feat", name: "feat:     新增功能 | A new feature" },
       { value: "fix", name: "fix:      修复缺陷 | A bug fix" },
-      { value: "docs", name: "docs:     文档更新 | Documentation only changes" },
-      { value: "style", name: "style:    代码格式 | Changes that do not affect the meaning of the code" },
-      { value: "refactor", name: "refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature" },
-      { value: "perf", name: "perf:     性能提升 | A code change that improves performance" },
-      { value: "test", name: "test:     测试相关 | Adding missing tests or correcting existing tests" },
-      { value: "build", name: "build:    构建相关 | Changes that affect the build system or external dependencies" },
+      {
+        value: "docs",
+        name: "docs:     文档更新 | Documentation only changes",
+      },
+      {
+        value: "style",
+        name: "style:    代码格式 | Changes that do not affect the meaning of the code",
+      },
+      {
+        value: "refactor",
+        name: "refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature",
+      },
+      {
+        value: "perf",
+        name: "perf:     性能提升 | A code change that improves performance",
+      },
+      {
+        value: "test",
+        name: "test:     测试相关 | Adding missing tests or correcting existing tests",
+      },
+      {
+        value: "build",
+        name: "build:    构建相关 | Changes that affect the build system or external dependencies",
+      },
       { value: "revert", name: "revert:   回退代码 | Revert to a commit" },
-      { value: "chore", name: "chore:    其他修改 | Other changes that do not modify src or test files" }
+      {
+        value: "chore",
+        name: "chore:    其他修改 | Other changes that do not modify src or test files",
+      },
     ],
     useEmoji: false,
     emojiAlign: "center",
@@ -143,7 +159,7 @@ module.exports = {
     issuePrefixs: [
       // 如果使用 gitee 作为开发管理
       { value: "link", name: "link:     链接 ISSUES 进行中" },
-      { value: "closed", name: "closed:   标记 ISSUES 已完成" }
+      { value: "closed", name: "closed:   标记 ISSUES 已完成" },
     ],
     customIssuePrefixsAlign: "top",
     emptyIssuePrefixsAlias: "skip",
@@ -158,6 +174,6 @@ module.exports = {
     defaultBody: "",
     defaultIssues: "",
     defaultScope: "",
-    defaultSubject: ""
-  }
+    defaultSubject: "",
+  },
 };
